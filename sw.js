f15f5c1796d26f22e0b13e5202d5fb5b0b4e36a6
ChrisKlegarYoag <gphpyoag@gmail.com>
2026-09-25 12:06:30 +0530

    Add linked web account and persistent PWA session

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='143-public-v3';
-const STATIC=['./','./index.html','./login.html','./download.html','./features.html','./privacy.html','./help.html','./assets/styles.css','./assets/app.js','./assets/qrcode.bundle.js','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png'];
+const CACHE='143-public-v4';
+const STATIC=['./','./index.html','./login.html','./web.html','./download.html','./features.html','./privacy.html','./help.html','./assets/styles.css','./assets/web.css','./assets/app.js','./assets/web.js','./assets/qrcode.bundle.js','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
