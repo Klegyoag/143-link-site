e5ab9e973c88cc749e3acea7a3dac524ef759046
ChrisKlegarYoag <gphpyoag@gmail.com>
2026-09-25 13:02:29 +0530

    Refresh linked web app scripts and cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='143-public-v4';
+const CACHE='143-public-v5';
 const STATIC=['./','./index.html','./login.html','./web.html','./download.html','./features.html','./privacy.html','./help.html','./assets/styles.css','./assets/web.css','./assets/app.js','./assets/web.js','./assets/qrcode.bundle.js','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
